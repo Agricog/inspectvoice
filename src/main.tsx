978b0e1105db493111e7f2dc5a23c85db95feff4
Agricog <mick@mickstevenson.com>
2026-02-23 05:24:55 +0000

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { App } from './App';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('Root element not found. Check index.html has <div id="root">.');
+}
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
+  </StrictMode>,
+);

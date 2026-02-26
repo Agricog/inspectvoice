f5c2f6b5b0df526017708981c67b91363080a597
Agricog <mick@mickstevenson.com>
2026-02-26 13:02:42 +0000

    Create mapbox-gl.d.ts

---
+++ src/mapbox-gl.d.ts
@@ -0,0 +1,9 @@
+// Stub for transitive type reference from mapbox-gl
+declare module '@mapbox/point-geometry' {
+  class Point {
+    x: number;
+    y: number;
+    constructor(x: number, y: number);
+  }
+  export default Point;
+}
